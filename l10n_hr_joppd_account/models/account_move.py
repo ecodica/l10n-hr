ec9600892a23445f8b6a82d31474663410ac17cf
gogy <goran.bogic@ecodica.eu>
2023-03-23 22:39:14 +0100

    [10012345;Ecodica;ADD] l10n_hr_joppd_account: adding new JOPPD account module.

---
+++ l10n_hr_joppd_account/models/account_move.py
@@ -0,0 +1,45 @@
+from odoo import fields, models, api
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    joppd_posted = fields.Boolean(string='JOPPD Posted',
+                                  readonly=True,
+                                  copy=False,
+                                  help='JOPPD Posted'
+                                  )
+
+    @api.depends('line_ids')
+    def _is_for_joppd(self):
+        for move in self:
+            for line in move.line_ids:
+                if line.is_for_joppd:
+                    move.is_for_joppd = True
+                    break
+
+    is_for_joppd = fields.Boolean(string='Is for JOPPD?', compute='_is_for_joppd')
+
+    def post_joppd(self, post_date=None):
+        joppd_entry_obj = self.env['l10n.hr.joppd.account.move.entry']
+        allowed_journal_types = {'cash', 'bank', 'purchase', 'general'}
+        for move in self.filtered(lambda m: m.is_for_joppd and m.journal_id.type in allowed_journal_types):
+            date_payment = post_date or move.date
+            if move.journal_id.type == 'general':
+                date_payment = False
+            for line in move.line_ids.filtered(lambda l: l.is_for_joppd):
+                entry_vals = dict(
+                    name=line.name or line.move_id.name,
+                    state=date_payment and 'ready' or 'wait',
+                    move_id=move.id,
+                    move_line_id=line.id,
+                    date=post_date or move.date,
+                    date_payment=date_payment,
+                    employee_id=line.employee_id.id,
+                    nontaxable_receipt_id=line.joppd_nontaxable_receipt_id.id,
+                    payment_method_id=line.joppd_payment_method_id.id,
+                    amount=line.debit - line.credit,
+                    company_id=move.company_id.id
+                )
+                joppd_entry_obj.create(entry_vals)
+            move.joppd_posted = True

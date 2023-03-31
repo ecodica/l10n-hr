66ebcb13e6b2335f76d1a0f79f9c601a4c57e0a9
gogy <goran.bogic@ecodica.eu>
2023-03-31 16:09:02 +0200

    [10012345;Mazars;IMP] l10n_hr_joppd_base: naming conventions, localization fields in Odoo base models ought to be prefixed with localization signature, in our case "l10n_hr".

--- l10n_hr_joppd_base/models/hr_employee.py
+++ l10n_hr_joppd_base/models/hr_employee.py
@@ -4,11 +4,11 @@ from odoo import fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    city_municipality_id = fields.Many2one(comodel_name='l10n.hr.joppd.code.register.city.municipality',
-                                           string='City/Municipality', ondelete='restrict',
-                                           domain=[('type', '=', 'normal')], groups="hr.group_hr_user",
-                                           tracking=True)
-    city_municipality_work_id = fields.Many2one(comodel_name='l10n.hr.joppd.code.register.city.municipality',
-                                                string='City/Municipality of Work', ondelete='restrict',
-                                                domain=[('type', '=', 'normal')], groups="hr.group_hr_user",
-                                                tracking=True)
+    l10n_hr_city_municipality_id = fields.Many2one(comodel_name='l10n.hr.joppd.code.register.city.municipality',
+                                                   string='City/Municipality', ondelete='restrict',
+                                                   domain=[('type', '=', 'normal')], groups="hr.group_hr_user",
+                                                   tracking=True)
+    l10n_hr_city_municipality_work_id = fields.Many2one(comodel_name='l10n.hr.joppd.code.register.city.municipality',
+                                                        string='City/Municipality of Work', ondelete='restrict',
+                                                        domain=[('type', '=', 'normal')], groups="hr.group_hr_user",
+                                                        tracking=True)

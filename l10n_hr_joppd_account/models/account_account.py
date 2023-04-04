a5e3e61a36b725a186391fb37c12ce2a703aa442
gogy <goran.bogic@ecodica.eu>
2023-04-04 13:31:57 +0200

    [10012345;Ecodica;REF] l10n_hr_joppd_account: rename of JOPPD fields, on Odoo base models naming fields with prefix "l10n_hr"

--- l10n_hr_joppd_account/models/account_account.py
+++ l10n_hr_joppd_account/models/account_account.py
@@ -4,13 +4,13 @@ from odoo import fields, models, api
 class AccountAccount(models.Model):
     _inherit = 'account.account'
 
-    joppd_nontaxable_receipt_id = fields.Many2one(
+    l10n_hr_joppd_nontaxable_receipt_id = fields.Many2one(
         comodel_name='l10n.hr.joppd.code.register.nontaxable.receipt',
         string='Non-taxable Receipt',
         ondelete='restrict',
         domain=[('type', '=', 'normal')],
         required=False)
-    joppd_payment_method_id = fields.Many2one(
+    l10n_hr_joppd_payment_method_id = fields.Many2one(
         comodel_name='l10n.hr.joppd.code.register.payment.method',
         string='Payment Method',
         ondelete='restrict',

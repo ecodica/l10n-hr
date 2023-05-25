4f80be78acfbcff9d6a570f703e5583653b3e171
gkliska <goran@ecodica.eu>
2023-05-25 13:10:29 +0200

    [l10n_hr] Croatian NKD is based on EU NACE 2.0

--- l10n_hr_nkd/models/__init__.py
+++ l10n_hr_nkd/models/__init__.py
@@ -1,3 +1,2 @@
-from . import l10n_hr_nkd
 from . import res_company
 from . import res_partner
--- l10n_hr_nkd/models/res_partner.py
+++ l10n_hr_nkd/models/res_partner.py
@@ -4,8 +4,6 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    l10n_hr_nkd_id = fields.Many2one(
-        comodel_name="l10n.hr.nkd",
-        string="NKD",
-        help="Main occupation classified according to NKD-2007",
+    nace_id = fields.Many2one(
+        help="Main occupation classified according to EU NACE 2.0 / NKD-2007",
     )

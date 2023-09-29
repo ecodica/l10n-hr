b58373b30e648377fbcd1ad9eb268e474757d209
gogy <goran.bogic@ecodica.eu>
2023-09-29 13:01:50 +0200

    [1185;Mazars;FIX] l10n_hr_account_asset: completely different logic for Croatian Asset depreciation(prorata=True and method_period_start='next').

--- l10n_hr_account_asset/models/account_asset.py
+++ l10n_hr_account_asset/models/account_asset.py
@@ -2,7 +2,7 @@
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
-
+from dateutil.relativedelta import relativedelta
 
 class AccountAssetAsset(models.Model):
     _inherit = "account.asset"
@@ -104,3 +104,93 @@ class AccountAssetAsset(models.Model):
         else:
             return super()._compute_depreciation_amount_per_fiscal_year(table, line_dates,
                                                                         depreciation_start_date, depreciation_stop_date)
+
+    def _compute_depreciation_table_lines(
+        self, table, depreciation_start_date, depreciation_stop_date, line_dates
+    ):
+        self.ensure_one()
+        if self.method_period_start != 'next' and not self.prorata:
+            return super()._compute_depreciation_table_lines(table, depreciation_start_date, depreciation_stop_date,
+                                                             line_dates)
+        currency = self.company_id.currency_id
+        asset_sign = 1 if self.depreciation_base >= 0 else -1
+        i_max = len(table) - 1
+        remaining_value = self.depreciation_base
+        depreciated_value = 0.0
+        company = self.company_id
+        fiscalyear_lock_date = company.fiscalyear_lock_date or fields.Date.to_date(
+            "1901-01-01"
+        )
+
+        for i, entry in enumerate(table):
+
+            lines = []
+            fy_amount_check = 0.0
+            fy_amount = entry["fy_amount"]
+            li_max = len(line_dates) - 1
+            prev_date = max(entry["date_start"], depreciation_start_date)
+            for li, line_date in enumerate(line_dates):
+                line_days = (line_date - prev_date).days + 1
+                if currency.is_zero(remaining_value):
+                    break
+
+                if line_date > min(entry["date_stop"], depreciation_stop_date) and not (
+                    i == i_max and li == li_max
+                ):
+                    prev_date = line_date
+                    break
+                else:
+                    prev_date = line_date + relativedelta(days=1)
+
+                if (
+                    self.method == "degr-linear"
+                    and currency.compare_amounts(
+                        asset_sign * (fy_amount - fy_amount_check), 0
+                    )
+                    < 0
+                ):
+                    break
+                # amount is always period_amount
+                # remaining_value is always subtracted with period_amount
+                amount = entry.get("period_amount")
+                remaining_value -= amount
+                fy_amount_check += amount
+                line = {
+                    "date": line_date,
+                    "days": line_days,
+                    "amount": amount,
+                    "depreciated_value": depreciated_value,
+                    "remaining_value": remaining_value,
+                    "init": fiscalyear_lock_date >= line_date,
+                }
+                print(line)
+                lines.append(line)
+                depreciated_value += amount
+
+            # Handle rounding and extended/shortened FY deviations.
+            #
+            # Remark:
+            # In account_asset_management version < 8.0.2.8.0
+            # the FY deviation for the first FY
+            # was compensated in the first FY depreciation line.
+            # The code has now been simplified with compensation
+            # always in last FT depreciation line.
+            # if self.method_time == "year" and not entry.get("day_amount"):
+            #     if not currency.is_zero(fy_amount_check - fy_amount):
+            #         diff = fy_amount_check - fy_amount
+            #         amount = amount - diff
+            #         remaining_value += diff
+            #         lines[-1].update(
+            #             {"amount": amount, "remaining_value": remaining_value}
+            #         )
+            #         depreciated_value -= diff
+
+            if not lines:
+                table.pop(i)
+            else:
+                entry["lines"] = lines
+            line_dates = line_dates[li:]
+
+        for entry in table:
+            if not entry["fy_amount"]:
+                entry["fy_amount"] = sum(line["amount"] for line in entry["lines"])

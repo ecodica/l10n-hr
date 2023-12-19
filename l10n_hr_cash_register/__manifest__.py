3a8de3f1da2e41301e2e43948dd4317f2c90f648
andro <andro.radic@ecodica.eu>
2023-12-19 19:23:01 +0100

    [1447;Mazars;ADD] l10n_hr_cash_register: adds cash register reports for Pay Slip and Payment Slip

---
+++ l10n_hr_cash_register/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Ecodica
+{
+    "name": """Cash Register""",
+    "summary": """Cash Register""",
+    "category": "Croatia",
+    "images": [],
+    "version": "16.0.0.0.1",
+    "application": False,
+    'author': "Ecodica",
+    "license": 'LGPL-3',
+    'website': "https://www.ecodica.eu",
+    "support": "support@ecodica.eu",
+
+    "depends": [
+        "account_accountant",
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+    "data": [
+        "report/cash_register_report.xml",
+        "views/account_bank_statement_line_views.xml",
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "auto_install": False,
+    "installable": True,
+}

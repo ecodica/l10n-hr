6dc50b4536ea2de30b1d2fc312dd3ce926f6153a
mcvrtila <mario.cvrtila@info3.hr>
2024-06-12 11:42:47 +0200

    [ADD] l10n_hr_joppd: init

---
+++ l10n_hr_joppd/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Ecodica
+{
+    "name": """JOPPD obrazac""",
+    "summary": """joppd obrazac osnova""",
+    "category": "Croatia",
+    "images": [],
+    "version": "17.0.1.0.0",
+    "application": False,
+    'author': "Ecodica",
+    "license": 'LGPL-3',
+    'website': "https://www.ecodica.eu",
+    "support": "support@ecodica.eu",
+    "licence": "AGPL-3",
+
+    "depends": [
+        "l10n_hr_base",
+        "l10n_hr_joppd_base",
+        "base_address_extended",  # Zbog adrese i kucnog broja!
+        "partner_firstname",
+        "date_range",
+        "report_xlsx_helper",
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+    "data": [
+        "security/joppd_security.xml",
+        "security/ir.model.access.csv",
+        "views/joppd_views.xml",
+        "views/joppd_menuitems.xml",
+        "report/joppd_obrazac_xlsx.xml",
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}

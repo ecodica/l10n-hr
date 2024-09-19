b6202c38893d4bc212575661faaf7ee32e573e59
pmihalic23 <patrik.mihalic@info3.hr>
2024-09-19 12:21:10 +0200

    [FIX] l10n_hr_account_fiskal: move submenu, add translations, unique constraint

--- l10n_hr_account_fiskal/models/pnp_category.py
+++ l10n_hr_account_fiskal/models/pnp_category.py
@@ -5,4 +5,8 @@ class PnpCategory(models.Model):
     _name = "l10n.hr.pnp.category"
     _description = "PNP Category"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
+    
+    _sql_constraints = [
+        ('unique_name', 'unique(name)', 'The name must be unique!')
+    ]

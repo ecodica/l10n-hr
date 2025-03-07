63889f0bc625bd4d2c10141e8b7920b2566aeebb
Luka <lvrabec.101@gmail.com>
2025-03-07 08:24:32 +0100

    [FIX] : l10n_hr_pain_credit_transfer : SEPA xml file generation changes

--- l10n_hr_pain_credit_transfer/models/account_payment_order.py
+++ l10n_hr_pain_credit_transfer/models/account_payment_order.py
@@ -31,8 +31,6 @@ class AccountPaymentOrder(models.Model):
     def generate_payment_file(self):  # noqa: C901
         """Creates the SEPA Credit Transfer file. That's the important code!"""
         self.ensure_one()
-        if self.payment_method_id.code.startswith('sepa_credit_transfer_hr'):
-            return super().generate_payment_file()
         pain_flavor = self.payment_method_id.pain_version
         # We use pain_flavor.startswith('pain.001.001.xx')
         # to support country-specific extensions such as

ed0b64410e296ef4905e180e3258244f8a4da14b
Luka Vrabec <89602694+lvrabec@users.noreply.github.com>
2025-03-21 11:02:47 +0100

    [FIX] i18n and minifix to sepa file name generation

--- l10n_hr_pain_base/models/account_payment_order.py
+++ l10n_hr_pain_base/models/account_payment_order.py
@@ -11,7 +11,7 @@ class AccountPaymentOrder(models.Model):
     def _get_file_prefix(self):
         self.ensure_one()
         date_today = datetime.today().strftime('%Y%m%d')
-        return 'UN' + date_today
+        return 'UN' + date_today + str(1).zfill(4)
 
     def compute_sepa_final_hook(self, sepa):
         self.ensure_one()

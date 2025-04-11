27b55844add171caf2d207ab4ccfbe9452718138
danijel <danijel.bosnar@ecodica.eu>
2025-04-11 14:27:50 +0200

    [;MAZARS;ADD] l10n_hr_l10n_rs_conflicts: add module which will prevent installation of certain l10n_rs modules

---
+++ l10n_hr_l10n_rs_conflicts/__manifest__.py
@@ -0,0 +1,27 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Croatia - Serbia localization conflicts",
+    "summary": "Avoid conflicts between Croatian and Serbian localization",
+    "category": "Localisation",
+    "images": [],
+    "version": "16.0.0.0.1",
+    "application": False,
+    "author": "Ecodica",
+    "website": "",
+    "license": "AGPL-3",
+    "excludes": [
+        "l10n_rs_account_asset",
+        "l10n_rs_edi_migration",
+        "l10n_rs_edi_test",
+        "l10n_rs_edi_vat_recording_test",
+        "l10n_rs_stock",
+        "l10n_rs_storno",
+        "l10n_rs_website_sale",
+    ],
+    "data": [],
+    "qweb": [],
+    "demo": [],
+    "auto_install": False,
+    "installable": True,
+}

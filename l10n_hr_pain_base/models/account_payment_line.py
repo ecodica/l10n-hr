f300f3975635dd1c057484302292272d14f388fc
gogy <goran.bogic@ecodica.eu>
2025-07-17 11:17:29 +0200

    [;Ecodica;IMP] l10n_hr_pain_base: multi-country improvements.

--- l10n_hr_pain_base/models/account_payment_line.py
+++ l10n_hr_pain_base/models/account_payment_line.py
@@ -7,8 +7,8 @@ class AccountPaymentLine(models.Model):
     _inherit = 'account.payment.line'
 
     def _default_communication_type(self):
-        # if country=="HR"
-        return 'HR ref'
+        if self.env.company.country_code == 'HR':
+            return 'HR ref'
 
     communication_type = fields.Selection(selection_add=[('HR ref', 'HR ref')], default=_default_communication_type,
                                           ondelete={"HR ref": "cascade"})

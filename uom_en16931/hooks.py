a886b8e9a293ffc2b08a78cffd2323e31d52165c
Goran Kliska <goran@ecodica.eu>
2025-10-09 17:43:46 +0200

    [ALL;MZS rs] Move module uom_en16931 to l10n_hr

---
+++ uom_en16931/hooks.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2025 - Ecodica d.o.o
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+def _pre_init_hook(cr):
+    cr.execute("""
+        UPDATE ir_model_data SET noupdate = FALSE
+            WHERE model = 'uom.uom' AND module = 'uom'
+    """)
+
+
+def _post_init_hook(cr, registry):
+    cr.execute("""
+        UPDATE ir_model_data SET noupdate = TRUE
+            WHERE model = 'uom.uom' AND module = 'uom'
+    """)

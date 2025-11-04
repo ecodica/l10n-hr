55f306193b800540d805268e6dec63df96acc4f8
gogy <goran.bogic@ecodica.eu>
2025-11-04 18:38:32 +0100

    [2651;Ecodica;IMP] l10n_hr_account_fiscal: new dependency, towards Odoo certificates module.

--- l10n_hr_account_fiscal/data/neutralize.sql
+++ l10n_hr_account_fiscal/data/neutralize.sql
@@ -1,5 +1,2 @@
--- remove fiscalization certificates
-DELETE FROM l10n_hr_fiscal_certificate;
-
 -- set automatic fiscalization to False
-UPDATE res_company SET l10n_hr_fiscal_on_confirm = False;
+UPDATE res_company SET l10n_hr_fiscal_test_env = True;

995b15a0cff10b54a2369c58817eae7d5ecd7cf2
gogy <goran.bogic@ecodica.eu>
2025-11-10 14:51:25 +0100

    [2651;Ecodica;IMP] l10n_hr_account_fiscal: extracting Subject VAT from certificate. Introducing new field in the certificate model and related Company.

--- l10n_hr_account_fiscal/models/certificate_certificate.py
+++ l10n_hr_account_fiscal/models/certificate_certificate.py
@@ -1,4 +1,8 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from cryptography import x509
+from cryptography.x509 import ObjectIdentifier
+from cryptography.hazmat.primitives.serialization import pkcs12
+import base64
 
 
 class Certificate(models.Model):
@@ -18,3 +22,35 @@ class Certificate(models.Model):
         readonly=False,
         string="FINA type"
     )
+    l10n_hr_subject_vat = fields.Char('Subject VAT', readonly=True)
+
+    @api.depends('content', 'pkcs12_password')
+    def _compute_pem_certificate(self):
+        res = super()._compute_pem_certificate()
+        for certificate in self:
+            content = base64.b64decode(certificate.with_context(bin_size=False).content)
+            cert = None
+            # Try to load the certificate in different format starting with DER then PKCS12 and
+            # finally PEM. If none succeeded, we report an error.
+            try:
+                cert = x509.load_der_x509_certificate(content)
+            except ValueError:
+                pass
+            if not cert:
+                try:
+                    pkcs12_password = certificate.pkcs12_password.encode(
+                        'utf-8') if certificate.pkcs12_password else None
+                    _key, cert, _additional_certs = pkcs12.load_key_and_certificates(content, pkcs12_password)
+                except ValueError:
+                    pass
+            if not cert:
+                try:
+                    cert = x509.load_pem_x509_certificate(content)
+                except ValueError:
+                    pass
+            try:
+                subject_vat = cert.subject.get_attributes_for_oid(ObjectIdentifier('2.5.4.97'))
+                certificate.l10n_hr_subject_vat = subject_vat[0].value if subject_vat else ""
+            except ValueError:
+                certificate.l10n_hr_subject_vat = None
+        return res

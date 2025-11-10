58e09e8ddeb07f242c2d485d451e7d9a2af72d16
Marko <marko.falatec@ecodica.eu>
2025-11-10 13:56:15 +0100

    [2740;KPD;FIX] l10n_hr_kpd: Add decorator and call super to execute previous onchange actions.

--- l10n_hr_kpd/__manifest__.py
+++ l10n_hr_kpd/__manifest__.py
@@ -10,7 +10,7 @@
     """,
     "category": "Croatia",
     "images": [],
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "application": False,
     'author': "Ecodica",
     "license": 'LGPL-3',

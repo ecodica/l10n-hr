9fd7b7a0fb88b757c702d274684ddd8c5a573a4f
Goran Kliska <goran@ecodica.eu>
2025-12-30 16:55:18 +0100

    [fiscal WIP] nape-odoo WIP, wrong user(GK), PyCharm has f... me once again

---
+++ l10n_hr_account_fiscal/migrations/18.0.3.0.0/post-migrate.py
@@ -0,0 +1,25 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+    """Update fiscal schema on fiskal certificate to the latest version."""
+
+    cr.execute("""
+        WITH
+            updated AS (
+                UPDATE
+                    l10n_hr_fiskal_certificate fc
+                SET
+                    fiskal_schema = split_part(fiskal_schema, '_', 1) || '_v1.10'
+                RETURNING
+                    fc.id
+            )
+        SELECT
+            COUNT(*) FROM updated;
+    """)
+
+    updated_count = cr.fetchone()[0]
+    _logger.info("--- MIGRATION SCRIPT COMPLETED: %s l10n.hr.fiskal.certificate records updated field fiskal_schema---", updated_count)
+
+    cr.commit()

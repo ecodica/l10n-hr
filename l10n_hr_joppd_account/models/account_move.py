d185baf666c5c975d96a12bc7f301dc3d39393f1
gogy <goran.bogic@ecodica.eu>
2026-04-02 17:07:44 +0200

    [;MZS;IMP,FIX,REF] l10n_hr_joppd_account: calculate date_payment from related employee reconciled lines, introducing Multi-Company filter of GL entries, resolving bug with unlinking of GL.

--- l10n_hr_joppd_account/models/account_move.py
+++ l10n_hr_joppd_account/models/account_move.py
@@ -9,6 +9,7 @@ class AccountMove(models.Model):
                                           copy=False,
                                           help='JOPPD Posted'
                                           )
+    l10n_hr_is_for_joppd = fields.Boolean(string='Is for JOPPD?', compute='_is_for_joppd')
 
     @api.depends('line_ids')
     def _is_for_joppd(self):
@@ -19,17 +20,19 @@ class AccountMove(models.Model):
                     move.l10n_hr_is_for_joppd = True
                     break
 
-    l10n_hr_is_for_joppd = fields.Boolean(string='Is for JOPPD?', compute='_is_for_joppd')
-
     def post_joppd(self, post_date=None):
         joppd_entry_obj = self.env['l10n.hr.joppd.account.move.entry']
         allowed_journal_types = {'cash', 'bank', 'purchase', 'general'}
         for move in self.filtered(lambda m: m.l10n_hr_is_for_joppd and m.journal_id.type in allowed_journal_types):
-            date_payment = post_date or move.date
-            if move.journal_id.type == 'general':
-                date_payment = False
             for line in move.line_ids.filtered(lambda l: l.l10n_hr_is_for_joppd):
+                date_payment = False
+                # filter out only for specific employee partner, resolving a possible case when move has more
+                # than a single employee JOPPD line
+                reconciled = line.move_id._get_reconciled_amls().filtered(lambda l: l.partner_id == line.partner_id)
+                if reconciled:
+                    date_payment = fields.first(reconciled).date
                 entry_vals = dict(
+                    company_id=move.company_id.id,
                     name=line.name or line.move_id.name,
                     state=date_payment and 'ready' or 'wait',
                     move_id=move.id,
@@ -39,8 +42,7 @@ class AccountMove(models.Model):
                     employee_id=line.l10n_hr_employee_id.id,
                     nontaxable_receipt_id=line.l10n_hr_joppd_nontaxable_receipt_id.id,
                     payment_method_id=line.l10n_hr_joppd_payment_method_id.id,
-                    amount=line.debit - line.credit,
-                    company_id=move.company_id.id
+                    amount=line.balance,
                 )
                 joppd_entry_obj.create(entry_vals)
             move.l10n_hr_joppd_posted = True

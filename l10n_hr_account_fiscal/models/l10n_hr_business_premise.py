a685605b9ce2ac2b7d4e827b47370e8afb48ba4d
gogy <goran.bogic@ecodica.eu>
2026-07-06 14:49:07 +0200

    [3249;l10n_Hr_v19;IMP,REF,FIX] l10n_hr_account_fiscal: prijaviRadnoVrijeme - code refactor. New fiscal wrapper for F1.0 API method calling.

--- l10n_hr_account_fiscal/models/l10n_hr_business_premise.py
+++ l10n_hr_account_fiscal/models/l10n_hr_business_premise.py
@@ -2,6 +2,7 @@ from odoo import fields, models, api, _
 from odoo.exceptions import UserError
 from ..fiscal import fiscal
 from datetime import date
+from .fiscal_wrapper import fisc_handler
 
 DATE_FORMAT = '%d.%m.%Y'
 
@@ -32,96 +33,74 @@ class L10nHrBusinessPremise(models.Model):
         string='Exception Working Hours',
         domain=[('type', '=', 'exception')],
     )
-    regular_working_hours_valid_from = fields.Date(string='Regular Working Hours Valid From')
-    regular_working_hours_valid_to = fields.Date(string='Regular Working Hours Valid To')
-    regular_working_hours_note = fields.Date(string='Regular Working Hours - Note')
-    exception_working_hours_valid_from = fields.Text(string='Exception Working Hours Valid From')
-    exception_working_hours_valid_to = fields.Date(string='Exception Working Hours Valid To')
-    exception_working_hours_note = fields.Text(string='Exception Working Hours - Note')
+    regular_working_hours_valid_from = fields.Date('Regular Working Hours Valid From', required=True)
+    regular_working_hours_note = fields.Text('Regular Working Hours Note', required=True)
 
-    def _prepare_fiscal_working_hours(self, factory, fiscal_data, msg_type):
+    def _handle_fisc_response(self, response, msg_type):
+        return None
+
+    def _prepare_set_fiscal_working_hours(self, factory):
         grouped_regular_hours, grouped_exception_hours = [], []
         if self.regular_working_hours_ids:
-            regular_hours = factory.type_factory.RedovnoType(
-                DatumOd=_fina_date_format(self.regular_working_hours_valid_from),
-                DatumDo=_fina_date_format(self.regular_working_hours_valid_to),
-                Napomena=self.regular_working_hours_note or None,
-            )
-            for regular in self.regular_working_hours_ids:
-                flavor = factory.type_factory.DvokratnoType if regular.split_shift else factory.type_factory.JednokratnoType
-                shift = flavor(
-                    DanUTjednu=regular.dow,
-                    RadnoVrijemeOd=regular.time_from,
-                    RadnoVrijemeDo=regular.time_to,
+            for valid_from, regulars in self.regular_working_hours_ids.grouped('valid_from').items():
+                # Get distinct descriptions by records, ought to be unique
+                desc = ', '.join(set(regulars.mapped(lambda r: r.description or '')))
+                regular_hours = factory.type_factory.RedovnoType(
+                    DatumOd=_fina_date_format(valid_from),
+                    Napomena=desc or None,
                 )
-                if regular.split_shift:
-                    regular_hours.Dvokratno.append(shift)
-                else:
-                    regular_hours.Jednokratno.append(shift)
-            grouped_regular_hours.append(regular_hours)
-
+                for regular in regulars:
+                    flavor = factory.type_factory.DvokratnoType if regular.split_shift else factory.type_factory.JednokratnoType
+                    shift = flavor(
+                        DanUTjednu=regular.dow,
+                        RadnoVrijemeOd=regular.time_from,
+                        RadnoVrijemeDo=regular.time_to,
+                    )
+                    if regular.split_shift:
+                        regular_hours.Dvokratno.append(shift)
+                    else:
+                        regular_hours.Jednokratno.append(shift)
+                grouped_regular_hours.append(regular_hours)
         if self.exception_working_hours_ids:
-            exception_hours = factory.type_factory.IznimkaType(
-                DatumOd=_fina_date_format(self.exception_working_hours_valid_from),
-                DatumDo=_fina_date_format(self.exception_working_hours_valid_to),
-                Napomena=self.exception_working_hours_note or None
-            )
-            for exception in self.exception_working_hours_ids:
-                flavor = factory.type_factory.DvokratnoType if exception.split_shift else factory.type_factory.JednokratnoType
-                shift = flavor(
-                    DanUTjednu=regular.dow,
-                    RadnoVrijemeOd=regular.time_from,
-                    RadnoVrijemeDo=regular.time_to,
+            for valid_on, exceptions in self.exception_working_hours_ids.grouped('valid_on').items():
+                exception_hours = factory.type_factory.IznimkeType(
+                    Datum=_fina_date_format(valid_on),
                 )
-                if regular.split_shift:
-                    exception_hours.Dvokratno.append(shift)
-                else:
-                    exception_hours.Jednokratno.append(shift)
-            grouped_exception_hours.append(exception_hours)
+                # Dunno why zeep Client types behave this inconsistent
+                if exception_hours.Jednokratno is None:
+                    exception_hours.Jednokratno = []
+                for exception in exceptions:
+                    flavor = factory.type_factory.DvokratnoType if exception.split_shift else factory.type_factory.JednokratnoType
+                    shift = flavor(
+                        DanUTjednu=exception.dow,
+                        RadnoVrijemeOd=exception.time_from,
+                        RadnoVrijemeDo=exception.time_to,
+                    )
+                    if exception.split_shift:
+                        exception_hours.Dvokratno.append(shift)
+                    else:
+                        exception_hours.Jednokratno.append(shift)
+                grouped_exception_hours.append(exception_hours)
 
         poslovni_prostor = factory.type_factory.PoslovniProstorType(
-            Oib=fiscal_data['cert_vat'],
+            Oib=self.company_id.company_registry,
             OznPosPr=self.l10n_hr_fiscal_code,
             RadnoVrijeme=factory.type_factory.RadnoVrijemeType(grouped_regular_hours + grouped_exception_hours)
         )
         return poslovni_prostor
 
-    def _handle_fisc_response(self, response, msg_type):
-        return True
+    def _prepare_get_fiscal_working_hours(self, factory, fiscal_data, msg_type):
+        pass
 
     def button_l10n_hr_test_fiscal_echo(self):
         self.company_id.button_l10n_hr_test_fiscal_echo(self)
 
+    @fisc_handler(msg_type='prijaviRadnoVrijeme')
     def button_l10n_hr_register_working_hours(self):
-        msg_type = 'prijaviRadnoVrijeme'
-        time_start = self.company_id.get_l10n_hr_time_formatted()
         fiscal_data = self.company_id.get_fiscal_data()
-        fisk = fiscal.Fiscalization(data=fiscal_data)
-        try:
-            service_proxy = fisk.client.service[msg_type]
-        except:
-            raise UserError(_("Service proxy %s not found", msg_type))
-
+        fisk = fiscal.Fiscalization(fiscal_data)
         zaglavlje = fisk.create_request_header()
-        working_hours = self._prepare_fiscal_working_hours(fisk, fiscal_data, msg_type)
-        req_kw = dict(Zaglavlje=zaglavlje, PoslovniProstor=working_hours,
-                      OibOper=self.env.user.company_registry)
-        response = None
-        odoo_error = {}
-        try:
-            response = fisk._call_service(service_proxy, req_kw)
-            self.company_id.create_fiscal_log(msg_type, fisk, response, time_start, self)
-            self._handle_fisc_response(response, msg_type)
-        except AttributeError as e:
-            odoo_error = {'error_message': str(e) + '\n' + str(e.obj)}
-        except Exception as e:
-            # NOTE: handle cases when response is not received from FINA
-            odoo_error = {'error_message': e.args[0]}
-        # log odoo error
-        if odoo_error.get('error_message'):
-            self.company_id.create_fiscal_log(msg_type, fisk, odoo_error, time_start, self)
-        # raise error
-        error_message = response and hasattr(response, 'error_message') and response['error_message'] or odoo_error.get(
-            'error_message')
-        if error_message and not self.company_id.l10n_hr_fiscal_silent_error_logging:
-            raise UserError(_("Fiscalization Error:\n %s") % error_message)
+        working_hours = self._prepare_set_fiscal_working_hours(fisk)
+        fisc_data = dict(Zaglavlje=zaglavlje, PoslovniProstor=working_hours,
+                         OibOper=self.env.user.company_registry)
+        return fisc_data
